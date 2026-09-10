f43391dd24975c9c547f72cf1958607282cc5f9c
phamgiakhanh2806-tech <phamgiakhanh2806@gmail.com>
2026-09-10 20:54:00 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,11 @@
 pluginManagement {
     repositories {
-        maven {
+        maven("https://maven.fabricmc.net/") {
             name = "Fabric"
-            url = uri("https://maven.fabricmc.net/")
         }
         mavenCentral()
         gradlePluginPortal()
     }
 }
 
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
-
-rootProject.name = "addon-template"
+rootProject.name = "amethyst-client"
